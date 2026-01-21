6c4d0391a66cca02ae075fbaf00ef4a886bb336e
Resurrect29 <9630305327@mail.ru>
2026-01-21 14:39:26 +0400

    add exception to task2

--- task2/src/Task2.java
+++ task2/src/Task2.java
@@ -1,6 +1,5 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.PrimitiveIterator;
 import java.util.Scanner;
 
 public class Task2 {
@@ -9,28 +8,63 @@ public class Task2 {
     private static final double EPSILON = 1e-10;
 
     public void main(String[] args) throws FileNotFoundException {
+
+        if (args.length < 2) {
+            System.err.println("Укажите пути к файлам как аргументы командной строки");
+            return;
+        }
         String ellipseFilePath = args[0];
         String pointsFilePath = args[1];
 
         Scanner scanner = new Scanner(new File(ellipseFilePath));
+
         String line = scanner.nextLine();
         String[] parts = line.split(" ");
-        centerX = Integer.parseInt(parts[0]);
-        centerY = Integer.parseInt(parts[1]);
+        if (parts.length < 2) {
+            System.err.println("Некорректный формат строки: " + line);
+            return;
+        }
+        try {
+            centerX = Integer.parseInt(parts[0]);
+            centerY = Integer.parseInt(parts[1]);
+        } catch (NumberFormatException e) {
+            System.err.println("Нечисловое значение в файле: " + line);
+            return;
+        }
 
         line = scanner.nextLine();
         parts = line.split(" ");
-        semiaxesA = Integer.parseInt(parts[0]);
-        semiaxesB = Integer.parseInt(parts[1]);
+        if (parts.length < 2) {
+            System.err.println("Некорректный формат строки: " + line);
+            return;
+        }
+        try{
+            semiaxesA = Integer.parseInt(parts[0]);
+            semiaxesB = Integer.parseInt(parts[1]);
+        } catch (NumberFormatException e){
+            System.err.println("Нечисловое значение в файле: " + line);
+            return;
+        }
+
 
         scanner = new Scanner(new File(pointsFilePath));
         while(scanner.hasNextLine()){
             line = scanner.nextLine();
             parts = line.split(" ");
-            int x = Integer.parseInt(parts[0]);
-            int y = Integer.parseInt(parts[1]);
+            if (parts.length < 2) {
+                System.err.println("Некорректный формат строки: " + line);
+                return;
+            }
 
-            System.out.println(checkPointLocation(x,y));
+            try{
+                int x = Integer.parseInt(parts[0]);
+                int y = Integer.parseInt(parts[1]);
+
+                System.out.println(checkPointLocation(x,y));
+            } catch (NumberFormatException e){
+                System.err.println("Нечисловое значение в файле: " + line);
+                return;
+            }
         }
     }
 
